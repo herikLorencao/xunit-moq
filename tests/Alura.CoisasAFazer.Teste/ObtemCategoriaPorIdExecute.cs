0016b81ae50592d47d41ee2cfac0d651794d74ac
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-02-09 14:30:41 -0300

    adicionando mais um exemplo com verify

---
+++ tests/Alura.CoisasAFazer.Teste/ObtemCategoriaPorIdExecute.cs
@@ -0,0 +1,26 @@
+﻿using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Infrastructure;
+using Alura.CoisasAFazer.Services.Handlers;
+using Moq;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Teste
+{
+    public class ObtemCategoriaPorIdExecute
+    {
+        [Fact]
+        public void QuandoIdExistenteDeveChamarObtemCategoriaUmaVez()
+        {
+            var mock = new Mock<IRepositorioTarefas>();
+            var repo = mock.Object;
+            
+            const int idCategoria = 20;
+            var comando = new ObtemCategoriaPorId(idCategoria);
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+
+            handler.Execute(comando);
+            
+            mock.Verify(r => r.ObtemCategoriaPorId(idCategoria), Times.Once());
+        }
+    }
+}
